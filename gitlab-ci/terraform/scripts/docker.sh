6cc402f0a8be82b53a38cb396d3dacf52fc5d266
Aleksandr Demshin <demshin@gmail.com>
2020-01-22 16:14:37 +0500

    simplify

--- gitlab-ci/terraform/scripts/docker.sh
+++ gitlab-ci/terraform/scripts/docker.sh
@@ -19,14 +19,3 @@ sudo apt update
 sudo apt-get --yes install docker-ce docker-compose
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker
-printf '\nDocker installed successfully\n\n'
-printf 'Waiting for Docker to start...\n\n'
-sleep 3
-
-# # Docker Compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-# sudo chmod +x /usr/local/bin/docker-compose \
-# sudo wget \
-#         --output-document=/etc/bash_completion.d/docker-compose \
-# "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose" \
-# && printf '\nDocker Compose installed successfully\n\n'
